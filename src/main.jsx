c1cba98a34264977d3bac67e538368bdde646a22
Chizoba Emeghiebo <chizobaemeh09@gmail.com>
2025-02-21 21:45:07 +0100

    resolved detector api on mobile issues

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 
 const originMeta1 = document.createElement("meta");
-const originMeta2 = document.createElement("meta");
-const originMeta3 = document.createElement("meta");
 originMeta1.httpEquiv = "origin-trial";
-originMeta1.content = import.meta.env.VITE_SUMMARIZER_API_KEY;
-originMeta2.httpEquiv = "origin-trial";
-originMeta2.content = import.meta.env.VITE_DETECTOR_API_KEY;
-originMeta3.httpEquiv = "origin-trial";
-originMeta3.content = import.meta.env.VITE_TRANSLATOR_API_KEY;
-
+originMeta1.content = import.meta.env.VITE_SUMMARIZER_TOKEN;
 document.head.append(originMeta1);
+
+const originMeta2 = document.createElement("meta");
+originMeta2.httpEquiv = "origin-trial";
+originMeta2.content = import.meta.env.VITE_DETECTOR_TOKEN;
 document.head.append(originMeta2);
+
+const originMeta3 = document.createElement("meta");
+originMeta3.httpEquiv = "origin-trial";
+originMeta3.content = import.meta.env.VITE_TRANSLATOR_TOKEN;
 document.head.append(originMeta3);
 
 createRoot(document.getElementById("root")).render(
